cc8fa19636b082ae2be422a11bda4ab048309369
fady emil <fady46t45f@gmail.com>
2024-06-29 19:49:49 +0300

    add Swiper Widget

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,9 +1,12 @@
 // import 'package:e_commerce_app/const/global_colors.dart';
 // import 'package:e_commerce_app/widget/app/AppBarIcons.dart';
+import 'package:card_swiper/card_swiper.dart';
+// import 'package:e_commerce_app/widget/app/AppBarIcons.dart';
 import 'package:e_commerce_app/widget/home/BarHomePAge.dart';
 import 'package:e_commerce_app/widget/home/SearchWidget.dart';
 import 'package:e_commerce_app/widget/home/sale_widget.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_iconly/flutter_iconly.dart';
 // import 'package:flutter_iconly/flutter_iconly.dart';
 
 class Homescreen extends StatelessWidget {
@@ -11,6 +14,7 @@ class Homescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return GestureDetector(
       onTap: () {
         //* Unfocus any focused input field
@@ -18,16 +22,53 @@ class Homescreen extends StatelessWidget {
       },
       child: Scaffold(
         appBar: BarHomePAge(),
-        body: const Padding(
-          padding: EdgeInsets.all(8.0),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               const SizedBox(height: 18),
-              SearchWidget(),
+              const SearchWidget(),
               const SizedBox(height: 18),
-              SaleWidget(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: SwiperWidget(),
+                      ),
+                     
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SwiperWidget extends StatelessWidget {
+  const SwiperWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Swiper(
+      itemCount: 3,
+      itemBuilder: (context, index) {
+        return SaleWidget();
+      },
+      autoplay: true,
+      pagination: SwiperPagination(
+        margin: EdgeInsets.all(8),
+        alignment: Alignment.bottomCenter,
+        builder: DotSwiperPaginationBuilder(
+          color: Colors.white,
+          activeColor: Colors.deepPurpleAccent,
         ),
       ),
     );
