35bb01c7701eb8207572c8de8cb77a82046fa39c
fady emil <fady46t45f@gmail.com>
2024-06-30 12:50:15 +0300

    move files in Product details sceen to product details widget

--- lib/screen/Product_Details.dart
+++ lib/screen/Product_Details.dart
@@ -1,8 +1,8 @@
 // import 'package:card_swiper/card_swiper.dart';
 // import 'package:e_commerce_app/const/global_colors.dart';
-import 'package:e_commerce_app/widget/Product_details/Head_details.dart';
-import 'package:e_commerce_app/widget/Product_details/List_image.dart';
-import 'package:e_commerce_app/widget/Product_details/desc.dart';
+import 'package:e_commerce_app/widget/Product_details_widget/Head_details.dart';
+import 'package:e_commerce_app/widget/Product_details_widget/List_image.dart';
+import 'package:e_commerce_app/widget/Product_details_widget/desc.dart';
 // import 'package:e_commerce_app/widget/app/ProdectWidget.dart';
 import 'package:flutter/material.dart';
 
--- lib/widget/Product_details_widget/desc.dart
+++ lib/widget/Product_details_widget/desc.dart
@@ -7,7 +7,7 @@ class desc extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
@@ -17,10 +17,10 @@ class desc extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        const SizedBox(
+        SizedBox(
           height: 18,
         ),
-        const Text(
+        Text(
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
           textAlign: TextAlign.start,
           style: TextStyle(fontSize: 25),
